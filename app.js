5a48d821018717ab0b7f62517e0a0e853ea4d725
laparfumerie <laparfumerie.asbl@gmail.com>
2026-06-17 00:11:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const SUPABASE_URL = "https://ybbpeowdmtjdmrbbnqsg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InliYnBlb3dkbXRqZG1yYmJucXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MTgwMzAsImV4cCI6MjA5NzE5NDAzMH0.fbWceQVHXiK6kxz3fRTqn6TP8_m0weFGu27-COU00Ts";
 
+// Initialisation du client Supabase
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Attente du chargement complet de la page
 document.addEventListener('DOMContentLoaded', () => {
